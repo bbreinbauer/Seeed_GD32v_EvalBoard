95103a17078408aaa2cb237ee662ad2fae514f18
bbreinbauer <bernhard.breinbauer@bildung.gv.at>
2021-10-24 21:32:25 +0200

    blink LED works

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,21 @@
 
 void setup() {
   // put your setup code here, to run once:
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PB1, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(PB1, HIGH);
+  // wait for a second
+  delay(500);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(PB1, LOW);
+  // wait for a second
+  delay(500);
 }
