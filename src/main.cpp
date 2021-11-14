5c33fbaaa22bef94e8a79dfa709fcd4bb1c1b42e
bbreinbauer <bernhard.breinbauer@bildung.gv.at>
2021-11-14 14:19:08 +0100

    fix inline assembler add extern assembler function

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
+extern "C" {
+  int asmAdd(int a, int b);
+  void asmSwap(int *a, int *b);
+}
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -20,20 +25,42 @@ void setup()
   tft.setCursor(8, 200);
   tft.print("Jipieieh!");
 
+  // test assembler interaction
+  int t = 5;
+  tft.setCursor(0, 70);
+  tft.print(t);
+  asm volatile("add %0, x0, x0" : "=r"(t));
+  tft.setCursor(20, 70);
+  tft.print(t);
+
+
   // reading info registers, but content does not make any sense
   int misa = 0, mvendorid = 0, marchid = 0;
-  asm volatile("csrr x0, misa" : "=r"(misa));
-  asm volatile("csrr x0, mvendorid" : "=r"(mvendorid));
-  asm volatile("csrr x0, marchid" : "=r"(marchid));
+  asm volatile("csrr %0, misa" : "=r"(misa));
+  asm volatile("csrr %0, mvendorid" : "=r"(mvendorid));
+  asm volatile("csrr %0, marchid" : "=r"(marchid));
   tft.setCursor(0, 9);
   tft.print(misa, HEX);
   tft.setCursor(0, 30);
   tft.print(mvendorid, HEX);
-  tft.setCursor(0, 60);
+  tft.setCursor(0, 50);
   tft.print(marchid, HEX);
 
+  int r = asmAdd(1, 2);
+  tft.setCursor(0, 90);
+  tft.print(r);
+
+  int a = 5, b = 20;
+  asmSwap(&a, &b);
+  tft.setCursor(0, 110);
+  tft.print(a);
+  tft.print(" ");
+  tft.print(b);
+
+
+
   // LCD functions(240 x 320)
-  // tft.fillCircle(50, 50, 20, TFT_DARKGREEN);
+  tft.fillCircle(50, 50, 20, TFT_DARKGREEN);
   // tft.drawCircle(100, 50, 20, TFT_DARKGREEN);
 
   // tft.fillRect(50, 100, 40, 20, TFT_DARKGREY);
